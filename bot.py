6b07370e9339c59207d0c2b85e2bbdb42d0caa79
billrunge <billrunge@gmail.com>
2021-09-02 21:47:06 +0200

    the bot now reads all incoming messages and looks for the longest word in a sentence with all distinct letters and adds that word in emojis reactions to the message

--- bot.py
+++ bot.py
@@ -3,14 +3,76 @@ import os
 
 import discord
 from dotenv import load_dotenv
+from collections import Counter
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
 client = discord.Client()
 
+intents = discord.Intents.none()
+intents.reactions = True
+intents.members = True
+intents.guilds = True
+
+emoji_letters = {
+    "A": "🇦",
+    "B": "🇧",
+    "C": "🇨",
+    "D": "🇩",
+    "E": "🇪",
+    "F": "🇫",
+    "G": "🇬",
+    "H": "🇭",
+    "I": "🇮",
+    "J": "🇯",
+    "K": "🇰",
+    "L": "🇱",
+    "M": "🇲",
+    "N": "🇳",
+    "O": "🇴",
+    "P": "🇵",
+    "Q": "🇶",
+    "R": "🇷",
+    "S": "🇸",
+    "T": "🇹",
+    "U": "🇺",
+    "V": "🇻",
+    "W": "🇼",
+    "X": "🇽",
+    "Y": "🇾",
+    "Z": "🇿"
+}
+
+
+def isUniqueChars(string):
+
+    freq = Counter(string)
+
+    if(len(freq) == len(string)):
+        return True
+    else:
+        return False
+
+
+def getLongestWordWithDistinctLetters(word_array):
+    longest_length = 0
+    longest_word = ''
+    for word in word_array:
+        word = ''.join(c for c in word if c.isalpha())
+        word = word.upper()
+        if (len(word) > longest_length and isUniqueChars(word)):
+            longest_word = word
+            longest_length = len(word)
+    return longest_word
+
+
 @client.event
-async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+async def on_message(message):
+    split_message = message.content.split()
+    emoji_word = getLongestWordWithDistinctLetters(split_message)
+    if len(emoji_word) > 4 and len(emoji_word) < 21:
+        for letter in emoji_word:
+            await message.add_reaction(emoji_letters[letter])
 
 client.run(TOKEN)
