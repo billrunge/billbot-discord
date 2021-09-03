ed5a5f6ff59e1f32e7c4f7125b928c6aee147e83
billrunge <billrunge@gmail.com>
2021-09-03 18:57:35 +0200

    Created folder for all on_message tasks and pulled the emoji letters logic into this folder

--- bot.py
+++ bot.py
@@ -4,66 +4,67 @@ import discord
 from dotenv import load_dotenv
 from collections import Counter
 from distutils.util import strtobool
+from on_message.emoji_letters import parseMessageAddEmoji
 
 load_dotenv()
 client = discord.Client()
 
-emoji_letters = {
-    "A": "🇦",
-    "B": "🅱️",
-    "C": "🇨",
-    "D": "🇩",
-    "E": "🇪",
-    "F": "🇫",
-    "G": "🇬",
-    "H": "🇭",
-    "I": "🇮",
-    "J": "🇯",
-    "K": "🇰",
-    "L": "🇱",
-    "M": "🇲",
-    "N": "🇳",
-    "O": "🇴",
-    "P": "🇵",
-    "Q": "🇶",
-    "R": "🇷",
-    "S": "🇸",
-    "T": "🇹",
-    "U": "🇺",
-    "V": "🇻",
-    "W": "🇼",
-    "X": "🇽",
-    "Y": "🇾",
-    "Z": "🇿"
-}
+# emoji_letters = {
+#     "A": "🇦",
+#     "B": "🅱️",
+#     "C": "🇨",
+#     "D": "🇩",
+#     "E": "🇪",
+#     "F": "🇫",
+#     "G": "🇬",
+#     "H": "🇭",
+#     "I": "🇮",
+#     "J": "🇯",
+#     "K": "🇰",
+#     "L": "🇱",
+#     "M": "🇲",
+#     "N": "🇳",
+#     "O": "🇴",
+#     "P": "🇵",
+#     "Q": "🇶",
+#     "R": "🇷",
+#     "S": "🇸",
+#     "T": "🇹",
+#     "U": "🇺",
+#     "V": "🇻",
+#     "W": "🇼",
+#     "X": "🇽",
+#     "Y": "🇾",
+#     "Z": "🇿"
+# }
 
 
-def isUniqueChars(string):
-    freq = Counter(string)
-    if(len(freq) == len(string)):
-        return True
-    else:
-        return False
+# def isUniqueChars(string):
+#     freq = Counter(string)
+#     if(len(freq) == len(string)):
+#         return True
+#     else:
+#         return False
 
 
-def getLongestWordWithDistinctLetters(word_array):
-    longest_length = 0
-    longest_word = ''
-    for word in word_array:
-        word = ''.join(c for c in word if c.isalpha())
-        word = word.upper()
-        if (len(word) > longest_length and isUniqueChars(word)):
-            longest_word = word
-            longest_length = len(word)
-    return longest_word
+# def getLongestWordWithDistinctLetters(word_array):
+#     longest_length = 0
+#     longest_word = ''
+#     for word in word_array:
+#         word = ''.join(c for c in word if c.isalpha())
+#         word = word.upper()
+#         if (len(word) > longest_length and isUniqueChars(word)):
+#             longest_word = word
+#             longest_length = len(word)
+#     return longest_word
 
 
-async def parseMessageAddEmoji(message):
-    split_message = message.content.split()
-    emoji_word = getLongestWordWithDistinctLetters(split_message)
-    if len(emoji_word) >= int(os.getenv('MIN_WORD_LENGTH')) and len(emoji_word) <= int(os.getenv('MAX_WORD_LENGTH')):
-        for letter in emoji_word:
-            await message.add_reaction(emoji_letters[letter])
+# async def parseMessageAddEmoji(message):
+#     split_message = message.content.split()
+#     emoji_word = getLongestWordWithDistinctLetters(split_message)
+#     if len(emoji_word) >= int(os.getenv('MIN_WORD_LENGTH')) and len(emoji_word) <= int(os.getenv('MAX_WORD_LENGTH')):
+#         for letter in emoji_word:
+#             await message.add_reaction(emoji_letters[letter])
 
 
 @client.event
---
+++ on_message/emoji_letters.py
@@ -0,0 +1,62 @@
+# emoji_letters.py
+import os
+import discord
+from dotenv import load_dotenv
+from collections import Counter
+
+load_dotenv()
+client = discord.Client()
+
+emoji_letter_dict = {
+    "A": "🇦",
+    "B": "🅱️",
+    "C": "🇨",
+    "D": "🇩",
+    "E": "🇪",
+    "F": "🇫",
+    "G": "🇬",
+    "H": "🇭",
+    "I": "🇮",
+    "J": "🇯",
+    "K": "🇰",
+    "L": "🇱",
+    "M": "🇲",
+    "N": "🇳",
+    "O": "🇴",
+    "P": "🇵",
+    "Q": "🇶",
+    "R": "🇷",
+    "S": "🇸",
+    "T": "🇹",
+    "U": "🇺",
+    "V": "🇻",
+    "W": "🇼",
+    "X": "🇽",
+    "Y": "🇾",
+    "Z": "🇿"
+}
+
+def isUniqueChars(string):
+    freq = Counter(string)
+    if(len(freq) == len(string)):
+        return True
+    else:
+        return False
+
+def getLongestWordWithDistinctLetters(word_array):
+    longest_length = 0
+    longest_word = ''
+    for word in word_array:
+        word = ''.join(c for c in word if c.isalpha())
+        word = word.upper()
+        if (len(word) > longest_length and isUniqueChars(word)):
+            longest_word = word
+            longest_length = len(word)
+    return longest_word    
+
+async def parseMessageAddEmoji(message):
+    split_message = message.content.split()
+    emoji_word = getLongestWordWithDistinctLetters(split_message)
+    if len(emoji_word) >= int(os.getenv('MIN_WORD_LENGTH')) and len(emoji_word) <= int(os.getenv('MAX_WORD_LENGTH')):
+        for letter in emoji_word:
+            await message.add_reaction(emoji_letter_dict[letter])
