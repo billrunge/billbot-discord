3cdca84dfe7284835b64f879cdea10f40105e5ab
billrunge <billrunge@gmail.com>
2021-09-03 21:11:58 +0200

    python files in on_message folder will be iterated through and execute(message) will execute

--- bot.py
+++ bot.py
@@ -1,10 +1,9 @@
 # bot.py
 import os
 import discord
-from dotenv import load_dotenv
-from collections import Counter
-from distutils.util import strtobool
 from on_message import *
+from dotenv import load_dotenv
+from importlib.machinery import SourceFileLoader
 
 load_dotenv()
 client = discord.Client()
@@ -12,18 +11,12 @@ client = discord.Client()
 
 @client.event
 async def on_message(message):
-    # if ('OBJECTION' in message.content.upper()) and bool(strtobool(os.getenv('CHANNEL_LIMITED'))) and str(message.channel.id) == os.getenv('CHANNEL_ID'):
-    #     await execute(message)
-    # elif ('OBJECTION' in message.content.upper()) and not bool(strtobool(os.getenv('CHANNEL_LIMITED'))):
-    #     await execute(message)
 
-    await emoji_letters.execute(message)
-    await objection.execute(message)
-
-    # if bool(strtobool(os.getenv('CHANNEL_LIMITED'))) and str(message.channel.id) == os.getenv('CHANNEL_ID'):
-    #     await on_message.emoji_letters.execute(message)
-    # elif not bool(strtobool(os.getenv('CHANNEL_LIMITED'))):
-    #     await parseMessageAddEmoji(message)
-
+    list_modules=os.listdir('on_message')
+    list_modules.remove('__init__.py')
+    for module_name in list_modules:
+        if module_name.split('.')[-1]=='py':
+            foo = SourceFileLoader(module_name, f'on_message/{module_name}').load_module()
+            await foo.execute(message)
 
 client.run(os.getenv('TOKEN'))
--- on_message/__init__.py
+++ on_message/__init__.py
@@ -1,1 +1,10 @@
-__all__ = ['objection', 'emoji_letters']
+from os.path import dirname, basename, isfile, join
+import glob
+
+
+modules = glob.glob(join(dirname(__file__), "*.py"))
+__all__ = [basename(f)[:-3] for f in modules if isfile(f)
+           and not f.endswith('__init__.py')]
+
+
+#__all__ = ['objection', 'emoji_letters']
