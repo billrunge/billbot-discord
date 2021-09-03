5a74e113f0ea0e95eefb4c0af39a7a72b2503a5e
billrunge <billrunge@gmail.com>
2021-09-04 01:30:11 +0200

    resetting file to create mergable version

--- bot.py
+++ bot.py
@@ -13,17 +13,11 @@ client = discord.Client()
 async def on_message(message):
 
     dir = 'on_message'
-    module_name = 'gif_finder.py'
-
-    module = SourceFileLoader(module_name, f'{dir}/{module_name}').load_module()
-    await module.execute(message)
-
-    # dir = 'on_message'
-    # list_modules=os.listdir(dir)
-    # list_modules.remove('__init__.py')
-    # for module_name in list_modules:
-    #     if module_name.split('.')[-1]=='py':
-    #         module = SourceFileLoader(module_name, f'{dir}/{module_name}').load_module()
-    #         await module.execute(message)
+    list_modules=os.listdir(dir)
+    list_modules.remove('__init__.py')
+    for module_name in list_modules:
+        if module_name.split('.')[-1]=='py':
+            module = SourceFileLoader(module_name, f'{dir}/{module_name}').load_module()
+            await module.execute(message)
 
 client.run(os.getenv('TOKEN'))
