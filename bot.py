6c3ea80d41ee338411017c0d5c9d7faeeceb5299
billrunge <billrunge@gmail.com>
2021-09-04 19:03:42 +0200

    made the feature enabled message a litte more fun

--- bot.py
+++ bot.py
@@ -38,29 +38,6 @@ async def on_message(message):
 
     if (str(message.content[0:8]).upper() == '!BILLBOT' ):
         await billbotExecute(message, all_modules, channel_modules)
-        # if ('FEATURES' in message.content.upper()):
-        #     for module in all_modules.find():
-        #         module_name = module["name"].rstrip('.py')
-        #         for channel_module in channel_modules.find():
-        #             if module_name == channel_module["module_name"]:
-        #                 module_name += ': Enabled'
-        #         await message.channel.send(module_name)
-        # if ('ENABLE'in message.content.upper()):
-        #     word_array = message.content.split()
-        #     for word in word_array:
-        #         for module in all_modules.find():
-        #             module_name = module["name"].rstrip('.py')
-        #             if (word.upper() == module_name.upper()):
-        #                 channel_modules.replace_one({'module_name': module_name, 'channel_id':  message.channel.id},
-        #                 {'module_name': module_name, 'channel_id':  message.channel.id}, upsert = True) 
-        # if ('DISABLE'in message.content.upper()):
-        #     word_array = message.content.split()
-        #     for word in word_array:
-        #         for module in all_modules.find():
-        #             module_name = module["name"].rstrip('.py')
-        #             if (word.upper() == module_name.upper()):
-        #                 channel_modules.delete_one({'module_name': module_name, 'channel_id':  message.channel.id})
-
     for module_object in all_modules.find():
         module_name = module_object["name"]
         type = module_object["type"]
