e87d0c69095663c005136423d53f6b06896c41d2
billrunge <billrunge@gmail.com>
2021-09-04 19:16:11 +0200

    minor formatting changes

--- billbot.py
+++ billbot.py
@@ -1,7 +1,7 @@
 import os
 import discord
 from discord import channel
-from modules.on_message import * 
+from modules.on_message import *
 from dotenv import load_dotenv
 from importlib.machinery import SourceFileLoader
 from database import get_database
@@ -9,6 +9,7 @@ from database import get_database
 load_dotenv()
 client = discord.Client()
 dbname = get_database()
+
 
 async def billbotExecute(message, all_modules, channel_modules):
 
@@ -19,18 +20,19 @@ async def billbotExecute(message, all_modules, channel_modules):
                 if module_name == channel_module["module_name"]:
                     module_name += ': enabled'
             await message.channel.send(module_name)
-    if ('ENABLE'in message.content.upper()):
+    if ('ENABLE' in message.content.upper()):
         word_array = message.content.split()
         for word in word_array:
             for module in all_modules.find():
                 module_name = module["name"].rstrip('.py')
                 if (word.upper() == module_name.upper()):
                     channel_modules.replace_one({'module_name': module_name, 'channel_id':  message.channel.id},
-                    {'module_name': module_name, 'channel_id':  message.channel.id}, upsert = True) 
-    if ('DISABLE'in message.content.upper()):
+                                                {'module_name': module_name, 'channel_id':  message.channel.id}, upsert=True)
+    if ('DISABLE' in message.content.upper()):
         word_array = message.content.split()
         for word in word_array:
             for module in all_modules.find():
                 module_name = module["name"].rstrip('.py')
                 if (word.upper() == module_name.upper()):
-                    channel_modules.delete_one({'module_name': module_name, 'channel_id':  message.channel.id})
+                    channel_modules.delete_one(
+                        {'module_name': module_name, 'channel_id':  message.channel.id})
