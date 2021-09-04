d9c2ed0b6db1c3cfb843e2e5078ca4e6bc5a116e
billrunge <billrunge@gmail.com>
2021-09-04 18:59:42 +0200

    pulling billbot logic into its own python file

--- billbot.py
+++ billbot.py
@@ -1,17 +1,36 @@
-# import os
-# import discord
-# from discord.ext import commands
-# from dotenv import load_dotenv
-# from discord_slash import SlashCommand, SlashContext
+import os
+import discord
+from discord import channel
+from modules.on_message import * 
+from dotenv import load_dotenv
+from importlib.machinery import SourceFileLoader
+from database import get_database
 
-# load_dotenv()
+load_dotenv()
+client = discord.Client()
+dbname = get_database()
 
-# bot = discord.Client(intents=discord.Intents.default())
-# slash = SlashCommand(bot)
+async def billbotExecute(message, all_modules, channel_modules):
 
-# @slash.slash(name="test")
-# async def test(ctx: SlashContext):
-#     embed = discord.Embed(title="Embed Test")
-#     await ctx.send(embed=embed)
-
-# bot.run(os.getenv('TOKEN'))
+    if ('FEATURES' in message.content.upper()):
+        for module in all_modules.find():
+            module_name = module["name"].rstrip('.py')
+            for channel_module in channel_modules.find():
+                if module_name == channel_module["module_name"]:
+                    module_name += ': Enabled'
+            await message.channel.send(module_name)
+    if ('ENABLE'in message.content.upper()):
+        word_array = message.content.split()
+        for word in word_array:
+            for module in all_modules.find():
+                module_name = module["name"].rstrip('.py')
+                if (word.upper() == module_name.upper()):
+                    channel_modules.replace_one({'module_name': module_name, 'channel_id':  message.channel.id},
+                    {'module_name': module_name, 'channel_id':  message.channel.id}, upsert = True) 
+    if ('DISABLE'in message.content.upper()):
+        word_array = message.content.split()
+        for word in word_array:
+            for module in all_modules.find():
+                module_name = module["name"].rstrip('.py')
+                if (word.upper() == module_name.upper()):
+                    channel_modules.delete_one({'module_name': module_name, 'channel_id':  message.channel.id})
