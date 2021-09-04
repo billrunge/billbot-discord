712139b33472a349079d2c90f7950dc380acb7bf
billrunge <billrunge@gmail.com>
2021-09-04 16:17:35 +0200

    billbot now reads the modules folder for different module category, and then parses those folder to popopulate a database and then reads from that database when running modules

--- bot.py
+++ bot.py
@@ -1,27 +1,46 @@
 # bot.py
 import os
 import discord
-from on_message import * 
+from discord import channel
+from modules.on_message import * 
 from dotenv import load_dotenv
 from importlib.machinery import SourceFileLoader
+from database import get_database
 
 load_dotenv()
 client = discord.Client()
+dbname = get_database()
+
+
+def loadModules():
+    all_modules_db = dbname["all_modules"]
+    all_modules_db.delete_many({})
+
+    modules_dir=os.listdir("modules")
+    for dir in modules_dir:
+        print(dir)
+        list_modules=os.listdir(f'modules/{dir}')
+        list_modules.remove('__init__.py')
+        for module_name in list_modules:
+            if module_name.split('.')[-1]=='py':
+                all_modules_db.insert_one({"name": module_name, "type": dir})
 
 # if os.getenv('bop') == None:
 #     print("test")
 
-
+loadModules()
 
 @client.event
 async def on_message(message):
 
-    dir = 'on_message'
-    list_modules=os.listdir(dir)
-    list_modules.remove('__init__.py')
-    for module_name in list_modules:
-        if module_name.split('.')[-1]=='py':
-            module = SourceFileLoader(module_name, f'{dir}/{module_name}').load_module()
+    all_modules = dbname["all_modules"]
+
+    for module_object in all_modules.find():
+        module_name = module_object["name"]
+        type = module_object["type"]
+        if type == 'on_message':
+            module = SourceFileLoader(module_name, f'modules/{type}/{module_name}').load_module()
             await module.execute(message)
 
 client.run(os.getenv('TOKEN'))
+
--- database.py
+++ database.py
@@ -1,7 +1,14 @@
+import os
 from pymongo import MongoClient
 from dotenv import load_dotenv
 load_dotenv()
 
-def database():
-    dbclient = MongoClient(os.getenv('MONGO_DB_CONNECTION_STRING'))
-    db = dbclient.test
+def get_database():
+    client = MongoClient(os.getenv('MONGO_DB_CONNECTION_STRING'))
+    return client['billbotdb']
+
+
+if __name__ == "__main__":    
+    
+    # Get the database
+    dbname = get_database()
