8914e243918b7b9ea1b2dd292323995bc98a0b26
billrunge <billrunge@gmail.com>
2021-09-04 21:31:08 +0200

    billbot features now returns all features in one message

--- billbot.py
+++ billbot.py
@@ -15,11 +15,13 @@ async def billbotExecute(message, all_modules, channel_modules):
 
     if ('FEATURES' in message.content.upper()):
         for module in all_modules.find():
+            modules = ''
             module_name = module["name"].rstrip('.py')
             for channel_module in channel_modules.find({'channel_id':  message.channel.id}):
                 if module_name == channel_module["module_name"]:
                     module_name += ': enabled'
-            await message.channel.send(module_name)
+            modules += f'{module_name} \n'
+        await message.channel.send(modules)
     if ('ENABLE' in message.content.upper()):
         word_array = message.content.split()
         for word in word_array:
